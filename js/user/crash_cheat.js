be18d9a7bf1e109d945355d481008c33808b9894
Brenda <Brenda_Xuthus@hotmail.com>
2020-05-31 01:03:46 +0800

    Site updated: 2020-05-31 01:03:42

---
+++ js/user/crash_cheat.js
@@ -0,0 +1,17 @@
+<!--title文字趣味变化-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         //$('[rel="icon"]').attr('href', "/images/favicon-16x16-next.png");
+         document.title = 'Σ(っ°Д°;)っ 别走呀';
+         clearTimeout(titleTime);
+     }
+     else {
+         //$('[rel="icon"]').attr('href', "/images/favicon-16x16-next.png");
+         document.title = '٩(๛ ˘ ³˘)۶ 哼 ' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
